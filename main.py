c9e08306b198c55f40015ee031b185bcf87a4ba8
BogdanDolia <cr.co.erph@gmail.com>
2025-12-03 23:58:59 +0200

    Add CircleShape and Player classes with basic drawing functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 VERSION = pygame.version.ver
 
@@ -11,14 +12,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
+        print(f"{dt}")
 
 
 if __name__ == "__main__":
